c99007bcd8a6ad57bbfcc31c957dd7bfce090100
Đinh Phúc Khang <khangphuc1@gmail.com>
2022-06-13 22:16:24 +0700

    create blockchain

--- blockchain/contracts/giaodichmuaban_vattu.sol
+++ blockchain/contracts/giaodichmuaban_vattu.sol
@@ -2,5 +2,38 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract GiaoDichMuaBan_VatTu {
-    
+
+    struct Giaodich {
+        uint id_giaodichmuaban_vattu;
+        address nhacungcapvattu;
+        address xavien;
+    }
+
+    event giaodichEvent( uint id_giaodichmuaban_vattu, address nhacungcap, address xavien );
+
+    mapping( uint => Giaodich ) public list_giaodich;
+
+    function themGiaoDich(address nhacungcap, uint id_giaodichmuaban_vattu) public returns (bool) {
+        
+        Giaodich memory giaodich;
+        giaodich =  Giaodich(
+                        id_giaodichmuaban_vattu, 
+                        nhacungcap, 
+                        msg.sender
+                    );
+        
+        emit giaodichEvent(id_giaodichmuaban_vattu, nhacungcap, msg.sender);
+
+        list_giaodich[id_giaodichmuaban_vattu] = giaodich;
+        
+        return true;
+    }
+
+    function layDanhSachGiaoDich() public returns (Giaodich[] memory list_giaodich) {
+
+        
+
+        return list_giaodich;
+    }
+
 }
