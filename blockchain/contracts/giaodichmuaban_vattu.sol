f6d650f456de9cf26258b0365d42f4cf11b66a9e
Đinh Phúc Khang <khangphuc1@gmail.com>
2022-06-14 19:43:37 +0700

    update blockchain

--- blockchain/contracts/giaodichmuaban_vattu.sol
+++ blockchain/contracts/giaodichmuaban_vattu.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract GiaoDichMuaBan_VatTu {
+    uint private length;
 
     struct Giaodich {
         uint id_giaodichmuaban_vattu;
@@ -9,9 +10,10 @@ contract GiaoDichMuaBan_VatTu {
         address xavien;
     }
 
+
     event giaodichEvent( uint id_giaodichmuaban_vattu, address nhacungcap, address xavien );
 
-    mapping( uint => Giaodich ) public list_giaodich;
+    mapping( address => mapping(uint => Giaodich) ) public list_giaodich;
 
     function themGiaoDich(address nhacungcap, uint id_giaodichmuaban_vattu) public returns (bool) {
         
@@ -24,16 +26,21 @@ contract GiaoDichMuaBan_VatTu {
         
         emit giaodichEvent(id_giaodichmuaban_vattu, nhacungcap, msg.sender);
 
-        list_giaodich[id_giaodichmuaban_vattu] = giaodich;
+        list_giaodich[msg.sender][id_giaodichmuaban_vattu] = giaodich;
         
         return true;
     }
 
-    function layDanhSachGiaoDich() public returns (Giaodich[] memory list_giaodich) {
+    function layDanhSachIdGiaoDich() public view returns (uint[] memory) {
+        uint[] memory list_id_giaodich;
+        uint index = 0;
 
-        
+        for(uint i = 0; i < list_giaodich[msg.sender].length; i++) {
+            list_id_giaodich[index] = list_giaodich[msg.sender][i].id_giaodichmuaban_vattu;
+            index++;
+        }
 
-        return list_giaodich;
+        return list_id_giaodich;
     }
 
 }
