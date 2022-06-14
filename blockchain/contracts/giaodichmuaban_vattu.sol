0481273a2b523a9ad98e60dfcd5ebd68dbfbec83
Đinh Phúc Khang <khangphuc1@gmail.com>
2022-06-14 20:48:30 +0700

    update blockchain

--- blockchain/contracts/giaodichmuaban_vattu.sol
+++ blockchain/contracts/giaodichmuaban_vattu.sol
@@ -2,45 +2,63 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract GiaoDichMuaBan_VatTu {
-    uint private length;
+    uint256 private length;
 
     struct Giaodich {
-        uint id_giaodichmuaban_vattu;
+        uint256 id_giaodichmuaban_vattu;
         address nhacungcapvattu;
         address xavien;
     }
 
+    event SuKienThemGiaoDich(
+        uint256 id_giaodichmuaban_vattu,
+        address nhacungcap,
+        address xavien
+    );
 
-    event giaodichEvent( uint id_giaodichmuaban_vattu, address nhacungcap, address xavien );
+    mapping(uint256 => Giaodich) public list_giaodich;
 
-    mapping( address => mapping(uint => Giaodich) ) public list_giaodich;
+    modifier kiemtraXacNhan(bool xacnhan) {
+        require(xacnhan == true, "Giao dich chua duoc xac nhan.");
 
-    function themGiaoDich(address nhacungcap, uint id_giaodichmuaban_vattu) public returns (bool) {
-        
+        _;
+    }
+
+    modifier kiemtraNhaCungCap(address nhacungcap) {
+        require(
+            nhacungcap != msg.sender,
+            "ID nha cung cap phai khac ID nong dan"
+        );
+
+        _;
+    }
+
+    function themGiaoDich(
+        address nhacungcap,
+        uint256 id_giaodichmuaban_vattu,
+        bool xacnhan
+    )
+        public
+        kiemtraXacNhan(xacnhan)
+        kiemtraNhaCungCap(nhacungcap)
+        returns (bool)
+    {
         Giaodich memory giaodich;
-        giaodich =  Giaodich(
-                        id_giaodichmuaban_vattu, 
-                        nhacungcap, 
-                        msg.sender
-                    );
-        
-        emit giaodichEvent(id_giaodichmuaban_vattu, nhacungcap, msg.sender);
 
-        list_giaodich[msg.sender][id_giaodichmuaban_vattu] = giaodich;
-        
+        giaodich = Giaodich(
+            id_giaodichmuaban_vattu, 
+            nhacungcap, 
+            msg.sender
+        );
+
+        emit SuKienThemGiaoDich(
+            id_giaodichmuaban_vattu, 
+            nhacungcap, 
+            msg.sender
+        );
+
+        list_giaodich[id_giaodichmuaban_vattu] = giaodich;
+
         return true;
     }
-
-    function layDanhSachIdGiaoDich() public view returns (uint[] memory) {
-        uint[] memory list_id_giaodich;
-        uint index = 0;
-
-        for(uint i = 0; i < list_giaodich[msg.sender].length; i++) {
-            list_id_giaodich[index] = list_giaodich[msg.sender][i].id_giaodichmuaban_vattu;
-            index++;
-        }
-
-        return list_id_giaodich;
-    }
-
 }
