63d7e03d2efae5181febee6bf68f7201645e9080
maitrongnhan001 <maitrongnhan.001@gmail.com>
2022-06-15 20:01:11 +0700

    test blockchain

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,36 +13,41 @@ const DB_host = process.env.DB_HOST || 'localhost'
 const DB_port = Number(process.env.DB_PORT) || 3306
 const DB_database = process.env.DB_DATABASE || 'argiculture'
 const DB_user = process.env.DB_USERNAME || 'root'
-const DB_password = process.env.DB_PASSWORD || '08062000aB'
+//const DB_password = process.env.DB_PASSWORD || '08062000aB'
 
 
 const init = async () => {
-    var web3 = new Web3('http://localhost:9545')
-    const contract = await new web3.eth.Contract(giaodichmuaban_lua.abi as any, '0x00dbe6Ee1dD36299F514a9673Ce2537AC9e32352');
-    await contract.methods.themGiaoDich('0x592dbf14c88466b62dd6c111990a22adf6aecdc2', 1)
-    .send({from: '0x33777f49939a0592e41ae8c5ea403acfb9900977'})
-   // const name = await contract.methods.ListGiaoDich(1).call()
+    var web3 = new Web3('http://localhost:9545/')
+    const contract = await new web3.eth.Contract(giaodichmuaban_lua.abi as any, '0xB64790785bB754B7f7BF45264472875CF5F60b39');
 
-   // console.log(name)
+    // const name1 = await contract.methods.ListGiaoDich(1).call()
+    // console.log(name1)
+
+    await contract.methods.themGiaoDich('0xD9a4CC8e5642AE73Aece38f250C36f6B4c92C109', 4)
+    .send({from: '0xbee2a0ebcb8a167832dc26eda166c3aeb2459e09'})
+
+    const name = await contract.methods.DanhSachGiaoDich('0xbee2a0ebcb8a167832dc26eda166c3aeb2459e09', 1).call()
+    console.log(name)
 }
 
 try {
 
     init()
 
-    app.use(cors())
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: true }))
+    // app.use(cors())
+    // app.use(express.json())
+    // app.use(express.urlencoded({ extended: true }))
 
-    //---------------route-------------//
-    createRouter(app)
+    // //---------------route-------------//
+    // createRouter(app)
 
-    //--------connect database---------//
-    createConnection(DB_host, DB_port, DB_database, DB_user, DB_password)
+    // //--------connect database---------//
+    // //createConnection(DB_host, DB_port, DB_database, DB_user, DB_password)
+    // createConnection(DB_host, DB_port, DB_database, DB_user, "")
 
-    app.listen(port, () => {
-        console.log('listening on port: ' + port)
-    })
+    // app.listen(port, () => {
+    //     console.log('listening on port: ' + port)
+    // })
 } catch (err) {
     throw err
 }
