ffb4bb605e90e4a7073c76da966d71d37b5cdf9d
maitrongnhan001 <maitrongnhan.001@gmail.com>
2022-06-17 01:08:28 +0700

    update truffle

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -7,12 +7,12 @@ module.exports = {
   //
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "192.168.1.3",
       port: 8545,
       network_id: "*"
     },
     test: {
-      host: "127.0.0.1",
+      host: "192.168.1.3",
       port: 8545,
       network_id: "*"
     }
