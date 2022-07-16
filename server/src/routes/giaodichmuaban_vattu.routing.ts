237681b6220375057b34f39f664d726314a71f80
Đinh Phúc Khang <khangphuc1@gmail.com>
2022-07-16 17:58:23 +0700

    create validator middleware

--- server/src/routes/giaodichmuaban_vattu.routing.ts
+++ server/src/routes/giaodichmuaban_vattu.routing.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import { Giaodichmuaban_vattuController } from '../controllers/giaodichmuaban_vattu.controller'
+import { GiaoDichMuaBanVatTuValidator} from '../middlewares/validator/GiaoDichMuaBanVatTu.middleware'
 const route = express.Router()
 
 const giaodichmuaban_vattuController = new Giaodichmuaban_vattuController
 
 route.get('/', giaodichmuaban_vattuController.getContracts)
 route.get('/:id', giaodichmuaban_vattuController.getContractById)
-route.post('/', giaodichmuaban_vattuController.addContract)
+route.post('/', GiaoDichMuaBanVatTuValidator as any, giaodichmuaban_vattuController.addContract)
 
 export default route
