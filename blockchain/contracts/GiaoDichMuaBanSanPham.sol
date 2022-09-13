6f519b3438aecd7572b1d3da21ff33c530b7f367
dpkhang <khangphuc1.dev@gmail.com>
2022-09-13 19:25:40 +0700

    create giaodichmuabansanpham contract

---
+++ blockchain/contracts/GiaoDichMuaBanSanPham.sol
@@ -0,0 +1,2 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.16 <0.9.0;
---
+++ blockchain/contracts/LoHangSanPham.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.16 <0.9.0;
+
+contract LoHangSanPham {
+    struct LoHangSanPham_Struct {
+        uint id_LoHangSanPham;
+        uint id_NguoiTieuDung;
+        uint id_NguoiBan;
+        uint SoLuong;
+        uint ThoiGian;
+    }
+
+    mapping(uint => LoHangSanPham_Struct) public DanhSachLoHangSanPham;
+
+    event SuKienThemLoHang(
+        uint id_LoHangSanPham,
+        uint id_NguoiTieuDung, 
+        uint id_NguoiBan,
+        uint SoLuong,
+        uint ThoiGian
+    );
+
+    modifier KiemTraIdLoHangSanPham (uint id_LoHangSanPham) {
+        require (
+            DanhSachLoHangSanPham[ id_LoHangSanPham ].id_LoHangSanPham == 0,
+            "Lo hang san pham da ton tai"
+        );
+        
+        _;
+    }
+
+    modifier KiemTraSoLuong ( uint SoLuong ) {
+        require(
+            SoLuong > 0,
+            "So luong phai lon hon 0"
+        );
+
+        _;
+    }
+
+        //-------handle------//
+    /*
+    intProperties [
+        0: uint            id_LoHangSanPham
+        1: uint            id_NguoiTieuDung,
+        2: uint            id_NguoiBan,
+        3: uint            SoLuong,
+        4: uint            ThoiGian
+    ]
+    */
+
+    function LuuThongTinLoHangSanPham (
+        uint[] memory intProperties
+    ) 
+    internal
+    returns (bool)
+    {
+        LoHangSanPham_Struct memory LoHangSanPhamMermory;
+        uint id_LoHangSanPham = intProperties[0];
+        uint id_NguoiTieuDung = intProperties[1];
+        uint id_NguoiBan = intProperties[2];
+        uint SoLuong = intProperties[3];
+        uint ThoiGian = intProperties[4];
+
+        LoHangSanPhamMermory = LoHangSanPham_Struct(
+            id_LoHangSanPham,
+            id_NguoiTieuDung,
+            id_NguoiBan,
+            SoLuong,
+            ThoiGian
+        );
+
+        DanhSachLoHangSanPham[ id_LoHangSanPham ] = LoHangSanPhamMermory;
+
+        emit SuKienThemLoHang(id_LoHangSanPham, id_NguoiTieuDung, id_NguoiBan, SoLuong, ThoiGian);
+
+        return true;
+    }
+
+    function ThemLoHangSanPham (
+        uint[]   memory intProperties
+    )
+    public
+    KiemTraIdLoHangSanPham(intProperties[0])
+    KiemTraSoLuong(intProperties[3])
+    returns(bool)
+    {
+        bool result = LuuThongTinLoHangSanPham(intProperties);
+        return result;
+    }
+
+    function LayThongTinSanPham(uint id_LoHangSanPham)
+    external view
+    returns (LoHangSanPham_Struct memory)
+    {
+        return DanhSachLoHangSanPham[ id_LoHangSanPham ];
+    }
+
+
+}
